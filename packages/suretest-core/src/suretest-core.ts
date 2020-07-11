bfa564d90393ab1c0c07d181057fc141b0f68fe0
CSSmitty <cssmittys@gmail.com>
2020-07-11 13:05:27 -0600

    feat: Constructing file tree with file nodes

--- packages/suretest-core/src/suretest-core.ts
+++ packages/suretest-core/src/suretest-core.ts
@@ -1,9 +1,8 @@
-import chalk = require('chalk')
-import { suretestTree } from '@suretest/suretest-tree'
+import SuretestTree from '@suretest/suretest-tree'
 
 import { fileSearch } from './suretest-filesearch'
 
 export const suretestCore = () => {
-    suretestTree()
-    console.warn(chalk.blue(fileSearch()))
+    const tree = new SuretestTree(fileSearch())
+    console.warn(tree)
 }
--- packages/suretest-tree/src/suretest-tree.ts
+++ packages/suretest-tree/src/suretest-tree.ts
@@ -1,3 +1,13 @@
-export const suretestTree = () => {
-    console.log('TESTING')
+import FileNode from './Nodes/FileNode'
+
+export default class SuretestTree {
+    public children: Array<FileNode>
+
+    constructor(filepaths: Array<string>) {
+        this.children = this._setupFileNodeChildren(filepaths)
+    }
+
+    private _setupFileNodeChildren = (filepaths: Array<string>) => {
+        return filepaths.map((filepath) => new FileNode(filepath))
+    }
 }
